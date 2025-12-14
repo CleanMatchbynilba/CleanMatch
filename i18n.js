9104e709378f1211e641b4cc0bd79fb3908d05f2
CleanMatchbynilba <cleanmatchbynilba@gmail.com>
2025-12-14 08:06:00 +0300

    i18n.js

---
+++ i18n.js
@@ -0,0 +1,10 @@
+export const TEXTS = {
+  en: {
+    title: "Trusted Cleaning Marketplace",
+    subtitle: "Cleaners, homeowners and agencies in one secure platform"
+  },
+  es: {
+    title: "Mercado confiable de limpieza",
+    subtitle: "Limpiadores, propietarios y agencias en una sola plataforma"
+  }
+};
